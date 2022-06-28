61f3193dee3445c051f3806e222dfb1e75090e2f
iniyarajes <105322364+iniyarajes@users.noreply.github.com>
2022-06-28 15:26:08 -0400

    Update Review.java

--- Review.java
+++ Review.java
@@ -2,32 +2,23 @@ import java.io.*;
 import java.util.*;
 import java.util.Scanner;
 //using factory method
-public class Review {
-    public static void main(String args[]){
-             String Review;
-             String choice;
-             System.out.print("Select R/r(Read), W/w(write): ");
-             choice = input.nextLine();
-          if(choice == R || choice == r){
-
-         readReview(Review);
+String choice;
+public interface Review {
+   void choice();
 }
-else if(choice == W || choice == w){
 
- writeReview(Review);
-}
-        
-    }
     
-    public static void readReview(String r)
-    {
-      return Review;
-        
+    public class Read implements Review{
+        public void choice() {
+      System.out.println(“review");
+   }
 
     }
-    public static void writeReview(String r){
+    public class Write implements Review{
+     public void choice() {
         System.out.print("Type Here: ");
              choice = input.nextLine();
+             }
     }
     
-}
+
