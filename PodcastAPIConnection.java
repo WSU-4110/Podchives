16ebffeb18acca34c7a479d53efa1362e31604f6
shreejakannan <61254914+shreejakannan@users.noreply.github.com>
2022-07-05 23:10:08 -0400

    HW 4 - Singleton - FR2
    Connecting to Podcast API using Singleton design pattern

---
+++ PodcastAPIConnection.java
@@ -0,0 +1,17 @@
+package com.podchives.draft2;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class PodcastAPIConnection {
+
+    @PostMapping ("/podapi")
+    private String getPodClient() {
+        String uri = "https://api.podchaser.com/graphql";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject(uri, restTemplate, String.class);
+    }
+
+}
