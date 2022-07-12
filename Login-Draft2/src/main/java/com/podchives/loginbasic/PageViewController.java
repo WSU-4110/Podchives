a240800ee041b862979c5c484e8517c5066137ae
shreejakannan <61254914+shreejakannan@users.noreply.github.com>
2022-07-12 12:07:58 -0400

    Add files via upload

---
+++ Login-Draft2/src/main/java/com/podchives/loginbasic/PageViewController.java
@@ -0,0 +1,35 @@
+package com.podchives.loginbasic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Controller
+public class PageViewController extends WebMvcConfigurerAdapter {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home.html";
+    }
+    @RequestMapping("/home")
+    public String home2() {
+        return "home.html";
+    }
+
+    @RequestMapping("/profile")
+    public String profile() {
+        return "profile.html";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/*");
+    }
+}
