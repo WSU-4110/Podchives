8c3d018728c3fdaf18fce58ec53ced4a0d27d9d5
shreejakannan <61254914+shreejakannan@users.noreply.github.com>
2022-07-13 23:13:05 -0400

    Delete Repositories directory

--- Repositories/Podcast/PodcastRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.mysqldraft3.Podcast;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PodcastRepository extends JpaRepository<Podcast, Long> {
-}
