072f064e6d7c2fc30737ea6e9daa085c6cdf8d48
shreejakannan <61254914+shreejakannan@users.noreply.github.com>
2022-07-17 02:16:10 -0400

    Update PodcastRepository.java

--- Podchives/src/main/java/com/project/podchives/Podcast/PodcastRepository.java
+++ Podchives/src/main/java/com/project/podchives/Podcast/PodcastRepository.java
@@ -1,6 +1,8 @@
-package com.example.mysqldraft3.Podcast;
+package com.project.podchives.Podcast;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PodcastRepository extends JpaRepository<Podcast, Long> {
+   @Query(value = "SELECT * FROM PODCASTS WHERE POD_URL=?1", nativeQuery = true)
+    Podcast findByPodUrl(String podUrl);
 }
