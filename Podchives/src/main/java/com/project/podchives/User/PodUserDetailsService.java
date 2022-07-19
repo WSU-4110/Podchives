6642bc614b4a7b6585d4999bb534843e7a4eecd5
shreejakannan <61254914+shreejakannan@users.noreply.github.com>
2022-07-19 15:44:04 -0400

    Update PodUserDetailsService.java

--- Podchives/src/main/java/com/project/podchives/User/PodUserDetailsService.java
+++ Podchives/src/main/java/com/project/podchives/User/PodUserDetailsService.java
@@ -5,8 +5,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class PodUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+            User user = userRepository.findByUsername(username);
+            if(user == null) {
+                throw new UsernameNotFoundException("User " + username + " not found.");
+            }
+            return new PodUserDetails(user);
     }
 }
