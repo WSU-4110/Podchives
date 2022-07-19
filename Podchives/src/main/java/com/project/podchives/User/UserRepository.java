b6328db46e164193ea3df83d7605585469331a9a
shreejakannan <61254914+shreejakannan@users.noreply.github.com>
2022-07-19 15:44:54 -0400

    Update UserRepository.java

--- Podchives/src/main/java/com/project/podchives/User/UserRepository.java
+++ Podchives/src/main/java/com/project/podchives/User/UserRepository.java
@@ -4,9 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-//    @Query("SELECT u FROM User u WHERE u.email = ?1")
-//    User findByEmail(String email);
-    // Query
+//    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    @Query(value = "SELECT * FROM USERS WHERE USERNAME=?1",nativeQuery = true)
+    User findByUsername(String username);
 
 }
